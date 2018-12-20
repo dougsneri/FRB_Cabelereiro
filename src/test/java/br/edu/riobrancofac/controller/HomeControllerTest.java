ac1af0cf67627a0b6081c363ac2cdddd76b46c49
dougsneri <dsds.douglas@gmail.com>
2018-12-20 00:46:31 -0200

    Ajustes

---
+++ src/test/java/br/edu/riobrancofac/controller/HomeControllerTest.java
@@ -0,0 +1,18 @@
+package br.edu.riobrancofac.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class HomeControllerTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testIniciar() {
+	}
+
+}
